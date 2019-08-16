f6a9771f4997158bb79b20f7ac04cb553cf21e7d
Allens Torres Ruíz <allenstr90@gmail.com>
2019-08-16 08:44:52 -0400

    [add] constants and system config

--- src/main/java/aem/component/security/oauth2server/config/SystemConfig.java
+++ src/main/java/aem/component/security/oauth2server/config/SystemConfig.java
@@ -1,8 +1,15 @@
 package aem.component.security.oauth2server.config;
 
-public class SystemConfig {
+public final class SystemConfig {
     public static final String BASE_PATH = "/api/v1";
+
     public static final String RESOURCE_ID = "oauth2Id";
+
+    public static final String ADMIN = "ROLE_ADMIN";
+
+    public static final String USER = "ROLE_USER";
+
+    public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private SystemConfig() {
     }
---
+++ src/main/java/aem/component/security/oauth2server/utils/Constants.java
@@ -0,0 +1,8 @@
+package aem.component.security.oauth2server.utils;
+
+public final class Constants {
+    public static final String USERNAME_PATTERN = "^[a-zA-z0-9]+$";
+
+    private Constants() {
+    }
+}
